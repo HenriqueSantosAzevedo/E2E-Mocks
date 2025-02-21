a3c33a8f4d3155792911f36a09b7f1de815a8ea0
Henrique Santos <henrique.santos.azevede@gmail.com>
2025-02-21 11:30:14 +0100

    Updates example

--- examples/index.ts
+++ examples/index.ts
@@ -1,10 +1,10 @@
 import {Context} from "hono";
-import {createState} from "../src/server/StateHandler";
+import {createState} from "e2e-mocks/src/server/StateHandler";
 
-type State = Array<{ id: number, name: string }>;
-const state = createState<State>('umsatz', () => [])
+type State = Array<{ id: number, title: string }>;
+const state = createState<State>('books', () => [])
 
-export const umsatz = [
+export const books = [
     {
         path: '',
         method: 'GET',
@@ -23,14 +23,14 @@ export const umsatz = [
             type: 'dynamic',
             async handler(context: Context) {
                 const id = context.req.param('id')
-                const index = state.getState().findIndex((umsatz) => umsatz.id == Number(id))
+                const index = state.getState().findIndex((book) => book.id == Number(id))
                 const body = await context.req.json()
 
                 if (index !== -1) {
                     state.getState()[index] = body
                     return Response.json(state.getState()[index])
                 } else {
-                    return Response.json({}, { status: 404 })
+                    return Response.json({}, {status: 404})
                 }
             }
         },
